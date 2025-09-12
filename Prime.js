f8de311e0e232e770a45fc1ed311850f362f3adc
bdushime <bdushime47@gmail.com>
2025-09-12 10:49:31 +0200

    exercise 3

--- Prime.js
+++ Prime.js
@@ -1,14 +1,12 @@
 function isPrime(num) {
   
-  if (num <= 1) {
-    return false;
-  }
+   if (num <= 1) return false;
 
-  for (let i = 2; i <= Math.sqrt(num); i++) {
+  for (let i = 2; i < num; i++) {
     if (num % i === 0) {
       return false; 
     }
   }
 
-  return true; 
+  return true;
 }
